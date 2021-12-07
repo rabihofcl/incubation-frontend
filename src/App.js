e06aa174544a4803347f11df7da46d10d2227c3f
Muhammed Rabih <rabihofcl@gmail.com>
2021-12-07 21:18:19 +0530

    logi access check success

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <Router>
-    <Switch>
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 
-    </Switch>
-  </Router>
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Layout from "./hocs/Layout";
+
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/reset-password' component={ResetPassword} />
+          <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+          <Route exact path='/activate/:uid/:token' component={Activate} />
+        </Switch>
+      </Layout>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ src/containers/ResetPasswordConfirm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResetPasswordConfirm() {
+    return (
+        <div>
+            ResetPasswordConfirm
+        </div>
+    )
+}
+
+export default ResetPasswordConfirm
